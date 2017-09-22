2dd6413ec4355b941a1c13cee52f7027862b3a26
yuzhu9 <poly2014w@163.com>
2017-09-22 10:56:34 +0800

    添加歌曲播放页面

---
+++ AV.js
@@ -0,0 +1,8 @@
+var APP_ID = '1luhcRP3UEcL6HFn0yykbN2E-gzGzoHsz';
+var APP_KEY = 'AVYfJvkkS0MhiiYe0pd9H8Wz';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+var query = new AV.Query('Song');
